d4cb5a033c09d6c7b222abecc7b4d9e2f38f5bdf
sno0wi <sno0wi20@gmail.com>
2023-12-12 18:59:47 +0200

    JS task-5 done

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const textSpan = document.querySelector(".color");
+
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  textSpan.textContent = randomColor;
+});
