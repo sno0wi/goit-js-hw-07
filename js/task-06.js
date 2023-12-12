6f4774c6f217215aaae4f1081a577a5270623b4a
sno0wi <sno0wi20@gmail.com>
2023-12-12 20:14:48 +0200

    JS task-6 done

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxesContainer = document.querySelector("#boxes");
+const createBtn = document.querySelector("[data-create]")
+const destroyBtn = document.querySelector("[data-destroy]")
+const input = document.querySelector("#controls input")
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100)  {
+    createBox(amount);
+    input.value = "";
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBox(amount) {
+  for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      boxesContainer.appendChild(box);
+    }
+}
+
+function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+  }
