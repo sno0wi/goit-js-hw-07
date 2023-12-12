397a9358da2c143c20b40c96838c8c580ab804f8
sno0wi <sno0wi20@gmail.com>
2023-12-12 16:16:52 +0200

    JS task-4 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("form")
+form.addEventListener("submit", formValidation);
+function formValidation(event) {
+    event.preventDefault();
 
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+        alert("Please enter your email and password")
+    }
+
+    console.log(`email: ${email.trim()}, Password: ${password.trim()}`);
+    form.reset();
+}
