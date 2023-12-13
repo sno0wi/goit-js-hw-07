a315d962908d115548e94d664ba0c02727760321
sno0wi <sno0wi20@gmail.com>
2023-12-13 12:07:20 +0200

    task-01 DONE

--- js/task-01.js
+++ js/task-01.js
@@ -6,3 +6,10 @@ categoryItems.forEach((item) => {
     const categoryElements = item.querySelectorAll('li');
     console.log(`Elements: ${categoryElements.length}`)
 });
+const body = document.querySelector("body");
+const categoriesEl = document.querySelector("#categories")
+categoriesEl.classList.add("categoriesList")
+const container = document.createElement("div");
+container.classList.add("container");
+container.append(categoriesEl);
+body.append(container)
